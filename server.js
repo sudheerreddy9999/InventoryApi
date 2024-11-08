7a84223cc68d23c501a0bda31bc799c8d4e2e116
sudheerreddy9999 <sudheerjanga9999@gmail.com>
2024-11-08 14:47:49 +0530

    api wakeup changes

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-'use strict'
+'use strict';
 
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 import helmet from 'helmet';
+import cron from 'node-cron';
 import Router from './routes/index.route.js';
 import appConfig from './config/app/app.config.js';
-import pgsql from './config/database/database.config.js'
+import pgsql from './config/database/database.config.js';
 
 const app = express();
 
@@ -15,26 +16,31 @@ app.use(cors());
 
 app.use(helmet());
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(Router);
 
-const authenticateDb =()=>{
-    try {
-        pgsql.authenticate();
-        console.log("Db Connected successfully")
-    } catch (error) {
-        console.error(error.message);
-    }
-}
-const startServer = ()=>{
-    try {
-        app.listen(appConfig.PORT,()=>{
-            console.log(`App listening on port ${appConfig.PORT}`);
-        }) 
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+const authenticateDb = () => {
+  try {
+    pgsql.authenticate();
+    console.log('Db Connected successfully');
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+const startServer = () => {
+  try {
+    app.listen(appConfig.PORT, () => {
+      console.log(`App listening on port ${appConfig.PORT}`);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+cron.schedule('*/50 * * * * *', () => {
+    console.log('Running the scheduled task...');
+    authenticateDb();
+  });
 startServer();
 authenticateDb();
